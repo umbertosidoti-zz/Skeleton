b132d4dcd2dde3c78117d338beec478556a09f72
Umbo <umbertosidoti@libero.it>
2019-08-11 23:15:26 +0700

    added network modulefirst success network call

--- app/src/main/java/com/umbo/skeleton/core/BaseViewModelLiveData.kt
+++ app/src/main/java/com/umbo/skeleton/core/BaseViewModelLiveData.kt
@@ -11,7 +11,7 @@ abstract class BaseViewModelLiveData<T>(): ViewModel() {
         }
     }
 
-    protected fun start() {
+    protected open fun start() {
 
     }
 }
--- app/src/main/java/com/umbo/skeleton/list/ListViewModel.kt
+++ app/src/main/java/com/umbo/skeleton/list/ListViewModel.kt
@@ -2,8 +2,20 @@ package com.umbo.skeleton.list
 
 import com.umbo.domain.ListInteractor
 import com.umbo.skeleton.core.BaseViewModelLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class ListViewModel @Inject constructor(interactor: ListInteractor): BaseViewModelLiveData<ListViewState>() {
+class ListViewModel @Inject constructor(private val interactor: ListInteractor): BaseViewModelLiveData<ListViewState>() {
     // TODO: Implement the ViewModel
+
+    override fun start() {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = interactor.posts()
+            liveData.postValue(ListViewState("test"))
+        }
+
+    }
 }
