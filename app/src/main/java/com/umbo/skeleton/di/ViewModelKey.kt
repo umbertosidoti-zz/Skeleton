d6421ca34f2b43e969f6d2a6b241822945231aa8
Umbo <umbertosidoti@libero.it>
2019-08-11 20:02:52 +0700

    changed viewmodelkey

--- app/src/main/java/com/umbo/skeleton/di/ViewModelKey.kt
+++ app/src/main/java/com/umbo/skeleton/di/ViewModelKey.kt
@@ -4,12 +4,6 @@ import androidx.lifecycle.ViewModel
 import dagger.MapKey
 import kotlin.reflect.KClass
 
-@MustBeDocumented
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
