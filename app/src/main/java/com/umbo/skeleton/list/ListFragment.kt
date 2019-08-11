c58b1f7bc39fc7c06c4382435edc16ee2b644e54
Umbo <umbertosidoti@libero.it>
2019-08-11 14:36:30 +0700

    added list fragment

---
+++ app/src/main/java/com/umbo/skeleton/list/ListFragment.kt
@@ -0,0 +1,29 @@
+package com.umbo.skeleton.list
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.umbo.skeleton.R
+
+
+class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
