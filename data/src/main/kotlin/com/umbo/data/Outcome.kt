8048c4d31bc22f2a00a049501e537894b9ca5241
Umbo <umbertosidoti@libero.it>
2020-02-01 16:37:02 +0700

    introduce okhhtp client

---
+++ data/src/main/kotlin/com/umbo/data/Outcome.kt
@@ -0,0 +1,6 @@
+package com.umbo.data
+
+sealed class Outcome<out T : Any> {
+    data class Success<out T : Any>(val value: T) : Outcome<T>()
+    data class Error(val message: String, val cause: Exception? = null) : Outcome<Nothing>()
+}
--- di/src/main/java/com/umbo/di/network/RetrofitModule.kt
+++ di/src/main/java/com/umbo/di/network/RetrofitModule.kt
@@ -1,23 +1,35 @@
 package com.umbo.di.network
 
 import com.umbo.data.NetworkService
-import com.umbo.network.RetrofitServiceImpl
 import com.umbo.network.RetrofitService
 import com.umbo.network.RetrofitService.Companion.BASE_URL
+import com.umbo.network.RetrofitServiceImpl
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 object RetrofitModule {
 
+    @Provides
+    @JvmStatic
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
     @Singleton
     @Provides
     @JvmStatic
-    fun provideRetrofitRepo(): NetworkService {
+    fun provideRetrofitRepo(client: OkHttpClient): NetworkService {
         val service = Retrofit.Builder()
+            .client(client)
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
             .build().create(RetrofitService::class.java)
