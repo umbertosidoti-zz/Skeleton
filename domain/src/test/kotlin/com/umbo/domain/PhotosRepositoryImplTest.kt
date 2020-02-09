c081a525e4f6c1d7ce5bdcb6d980f53c16c4e618
Umbo <umbertosidoti@libero.it>
2020-02-09 11:04:58 +0700

    added tests for repository

--- domain/src/test/kotlin/com/umbo/domain/PhotosRepositoryImplTest.kt
+++ domain/src/test/kotlin/com/umbo/domain/PhotosRepositoryImplTest.kt
@@ -1,8 +1,16 @@
 package com.umbo.domain
 
+import com.nhaarman.mockitokotlin2.whenever
 import com.umbo.data.NetworkService
+import com.umbo.data.Outcome
+import com.umbo.data.Photo
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.never
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
 class PhotosRepositoryImplTest {
@@ -19,5 +27,80 @@ class PhotosRepositoryImplTest {
     fun setUp() {
         MockitoAnnotations.initMocks(this)
         photosRepositoryImpl = PhotosRepositoryImpl(networkService, storage)
+    }
+
+    @Test
+    fun testPhotosFromCacheErrorEndNetworkError() = runBlocking {
+        //Given
+        whenever(storage.photos).thenReturn(Outcome.Error())
+        whenever(networkService.photos()).thenReturn(Outcome.Error())
+
+        //When
+        val photos = photosRepositoryImpl.photos()
+
+        //Then
+        verify(storage).photos
+        verify(networkService).photos()
+        assertTrue(photos is Outcome.Error)
+    }
+
+    @Test
+    fun testPhotosFromCacheEmptyEndNetworkError() = runBlocking {
+        //Given
+        whenever(storage.photos).thenReturn(Outcome.Success(emptyList()))
+        whenever(networkService.photos()).thenReturn(Outcome.Error())
+
+        //When
+        val photos = photosRepositoryImpl.photos()
+
+        //Then
+        verify(storage).photos
+        verify(networkService).photos()
+        assertTrue(photos is Outcome.Error)
+    }
+
+    @Test
+    fun testPhotosFromCache() = runBlocking {
+        //Given
+        whenever(storage.photos).thenReturn(Outcome.Success(listOf(Photo(1,1,"","",""))))
+        whenever(networkService.photos()).thenReturn(Outcome.Error())
+
+        //When
+        val photos = photosRepositoryImpl.photos()
+
+        //Then
+        verify(storage).photos
+        verify(networkService, never()).photos()
+        assertTrue(photos is Outcome.Success)
+    }
+
+    @Test
+    fun testPhotosFromNetworkWithError() = runBlocking {
+        //Given
+        whenever(storage.photos).thenReturn(Outcome.Success(listOf(Photo(1,1,"","",""))))
+        whenever(networkService.photos()).thenReturn(Outcome.Error())
+
+        //When
+        val photos = photosRepositoryImpl.photos(false)
+
+        //Then
+        verify(storage, never()).photos
+        verify(networkService).photos()
+        assertTrue(photos is Outcome.Error)
+    }
+
+    @Test
+    fun testPhotosFromNetwork() = runBlocking {
+        //Given
+        whenever(storage.photos).thenReturn(Outcome.Success(listOf(Photo(1,1,"","",""))))
+        whenever(networkService.photos()).thenReturn(Outcome.Success(emptyList()))
+
+        //When
+        val photos = photosRepositoryImpl.photos(false)
+
+        //Then
+        verify(storage, never()).photos
+        verify(networkService).photos()
+        assertTrue(photos is Outcome.Success)
     }
 }
