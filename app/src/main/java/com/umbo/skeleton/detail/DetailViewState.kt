04d81aadfd0b4e5ad6327c5c8bad50396fe27d47
Umbo <umbertosidoti@libero.it>
2020-02-27 23:40:42 +0700

    added jacoco coverage

--- app/src/main/java/com/umbo/skeleton/detail/DetailViewState.kt
+++ app/src/main/java/com/umbo/skeleton/detail/DetailViewState.kt
@@ -1,3 +1,3 @@
 package com.umbo.skeleton.detail
 
-class DetailViewState (val title: String, val url: String, val album: String)
+class DetailViewState(val title: String, val url: String, val album: String)
