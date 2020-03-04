b9762a47bea79ac9aa37e4338fe0e3eb71c20667
Umbo <umbertosidoti@libero.it>
2020-03-04 21:37:15 +0700

    created new module

---
+++ coverage_jacoco_plugin/src/main/kotlin/com/umbo/coveragejacoco/AndroidCoveragePlugin.kt
@@ -0,0 +1,12 @@
+package com.umbo.coveragejacoco
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidCoveragePlugin: Plugin<Project> {
+
+    override fun apply(prj: Project) {
+
+
+    }
+}
