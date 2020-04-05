9b75f12ce4ae51f0158647f0ce3d27340a992080
Umbo <umbertosidoti@libero.it>
2020-04-05 23:37:56 +0700

    fixed domain test

--- domain/src/main/kotlin/com/umbo/domain/interactor/DetailInteractorImpl.kt
+++ domain/src/main/kotlin/com/umbo/domain/interactor/DetailInteractorImpl.kt
@@ -2,12 +2,14 @@ package com.umbo.domain.interactor
 
 import com.umbo.data.*
 
-class DetailInteractorImpl(private val photosRepository: PhotosRepository,
-private val navigationPayloadRepository: NavigationPayloadRepository):
-    DetailInteractor {
+class DetailInteractorImpl(
+    private val photosRepository: PhotosRepository,
+    private val navigationPayloadRepository: NavigationPayloadRepository
+) : DetailInteractor {
 
     override suspend fun findPhoto(): Outcome<Photo> {
-        val id = (navigationPayloadRepository.payload as? DetailPayload)?.id ?: return Outcome.Error()
+        val id =
+            (navigationPayloadRepository.payload as? DetailPayload)?.id ?: return Outcome.Error()
         val outcome = photosRepository.photos()
         val photos = (outcome as? Outcome.Success)
         val selected = photos?.value?.find { id == it.id }
--- domain/src/test/kotlin/com/umbo/domain/DetailInteractorImpTest.kt
+++ domain/src/test/kotlin/com/umbo/domain/DetailInteractorImpTest.kt
@@ -1,10 +1,8 @@
 package com.umbo.domain
 
 import com.nhaarman.mockitokotlin2.whenever
-import com.umbo.data.Outcome
-import com.umbo.data.Photo
+import com.umbo.data.*
 import com.umbo.domain.interactor.DetailInteractorImpl
-import com.umbo.data.PhotosRepository
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Before
@@ -18,22 +16,26 @@ class DetailInteractorImpTest {
     @Mock
     lateinit var photosRepository: PhotosRepository
 
+    @Mock
+    lateinit var navigationPayloadRepository: NavigationPayloadRepository
+
     lateinit var detailInteractor: DetailInteractorImpl
 
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
         detailInteractor =
-            DetailInteractorImpl((photosRepository))
+            DetailInteractorImpl(photosRepository, navigationPayloadRepository)
     }
 
     @Test
     fun testReturnErrorIfErrorFromRepository() = runBlocking {
         //Given
+        whenever(navigationPayloadRepository.payload).thenReturn(DetailPayload(3))
         whenever(photosRepository.photos(anyBoolean())).thenReturn(Outcome.Error())
 
         //When
-        val result = detailInteractor.findPhoto(3)
+        val result = detailInteractor.findPhoto()
 
         //Then
         assertTrue(result is Outcome.Error)
@@ -42,10 +44,11 @@ class DetailInteractorImpTest {
     @Test
     fun testReturnErrorIfEmptyRepository() = runBlocking {
         //Given
+        whenever(navigationPayloadRepository.payload).thenReturn(DetailPayload(3))
         whenever(photosRepository.photos(anyBoolean())).thenReturn(Outcome.Success(emptyList()))
 
         //When
-        val result = detailInteractor.findPhoto(3)
+        val result = detailInteractor.findPhoto()
 
         //Then
         assertTrue(result is Outcome.Error)
@@ -54,10 +57,11 @@ class DetailInteractorImpTest {
     @Test
     fun testReturnErrorIfNoItemInRepository() = runBlocking {
         //Given
+        whenever(navigationPayloadRepository.payload).thenReturn(DetailPayload(3))
         whenever(photosRepository.photos()).thenReturn(Outcome.Success(listOf(Photo(1, 1, "", "", ""))))
 
         //When
-        val result = detailInteractor.findPhoto(3)
+        val result = detailInteractor.findPhoto()
 
         //Then
         assertTrue(result is Outcome.Error)
@@ -66,11 +70,12 @@ class DetailInteractorImpTest {
     @Test
     fun testReturnSuccess() = runBlocking {
         //Given
+        whenever(navigationPayloadRepository.payload).thenReturn(DetailPayload(3))
         val response = Outcome.Success(listOf(Photo(1, 3, "success", "", "")))
         whenever(photosRepository.photos()).thenReturn(response)
 
         //When
-        val result = detailInteractor.findPhoto(3)
+        val result = detailInteractor.findPhoto()
 
         //Then
         val photo = (result as? Outcome.Success)?.value
