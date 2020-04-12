9088db676e3c628da9f5dc6adf224d336477e5f3
Umbo <umbertosidoti@libero.it>
2020-04-12 11:33:29 +0700

    surrounded network call with try

--- network/src/main/kotlin/com/umbo/network/RetrofitNetworkServiceImpl.kt
+++ network/src/main/kotlin/com/umbo/network/RetrofitNetworkServiceImpl.kt
@@ -5,18 +5,22 @@ import com.umbo.network_interface.NetworkPhoto
 import com.umbo.network_interface.NetworkService
 
 
-class RetrofitNetworkServiceImpl(private val endPoint: RetrofitEndPoint, private val clientKey: String) :
-    NetworkService {
+class RetrofitNetworkServiceImpl(
+    private val endPoint: RetrofitEndPoint,
+    private val clientKey: String
+) : NetworkService {
 
     override suspend fun photos(): NetworkOutcome<List<NetworkPhoto>> {
-
-        val response = endPoint.getPhotos(clientKey)
-        val result = if (response.isSuccessful) {
-            response.body()
-        } else {
-            null
+        return try {
+            val response = endPoint.getPhotos(clientKey)
+            val result = if (response.isSuccessful) {
+                response.body()
+            } else {
+                null
+            }
+            if (result != null) NetworkOutcome.Success(result) else NetworkOutcome.Error()
+        } catch (e: Exception) {
+            NetworkOutcome.Error()
         }
-
-        return if (result != null) NetworkOutcome.Success(result) else NetworkOutcome.Error()
     }
 }
